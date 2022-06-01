030ce61234cf8613d54db4728f72a4f70d2f5c2a
McGuppy17 <e.c.w.torn@student.tue.nl>
2022-06-01 15:48:00 +0200

    some progress for save button

--- blockly/blocks_compressed.js
+++ blockly/blocks_compressed.js
@@ -71,7 +71,9 @@ Blockly.Blocks.grove_motor_shield={helpUrl:"http://www.seeedstudio.com/wiki/Moto
 null);this.setTooltip("Drive two brushed DC motors")}};
 Blockly.Blocks.grove_thumb_joystick={helpUrl:"http://www.seeedstudio.com/wiki/Grove_-_Thumb_Joystick",init:function(){this.setColour(190);this.appendDummyInput().appendField("Thumb Joystick").appendField(new Blockly.FieldImage("https://statics3.seeedstudio.com/images/product/bgjoy1.jpg",64,64)).appendField("PIN#").appendField(new Blockly.FieldDropdown(profile["default"].analog),"PIN").appendField("axis").appendField(new Blockly.FieldDropdown([["x","x"],
 ["y","y"]]),"AXIS");this.setOutput(!0,"Number");this.setTooltip("output two analog values(200~800) representing two directions")}};
-Blockly.Blocks.grove_rgb_led={helpUrl:"http://www.seeedstudio.com/wiki/index.php?title=Twig_-_Chainable_RGB_LED",init:function(){this.setColour(190);this.appendDummyInput().appendField("Chainable RGB LED").appendField(new Blockly.FieldImage("https://statics3.seeedstudio.com/images/product/chanbalelednb1.jpg",64,64)).appendField("PIN#").appendField(new Blockly.FieldDropdown(profile["default"].digital),"PIN");this.appendDummyInput("COLOR0").setAlign(Blockly.ALIGN_RIGHT).appendField("Color 1").appendField(new Blockly.FieldColour("#00ff00"),
+
+Blockly.Blocks.grove_rgb_led={helpUrl:"http://www.seeedstudio.com/wiki/index.php?title=Twig_-_Chainable_RGB_LED",
+init:function(){this.setColour(190);this.appendDummyInput().appendField("Chainable RGB LED").appendField(new Blockly.FieldImage("https://statics3.seeedstudio.com/images/product/chanbalelednb1.jpg",64,64)).appendField("PIN#").appendField(new Blockly.FieldDropdown(profile["default"].digital),"PIN");this.appendDummyInput("COLOR0").setAlign(Blockly.ALIGN_RIGHT).appendField("Color 1").appendField(new Blockly.FieldColour("#00ff00"),
 "RGB0");this.setMutator(new Blockly.Mutator(["grove_rgb_led_item"]));this.setPreviousStatement(!0,null);this.setNextStatement(!0,null);this.setTooltip("256 color LED, currently Chainable feature is not support");this.itemCount_=1},mutationToDom:function(){var a=document.createElement("mutation");a.setAttribute("items",this.itemCount_);for(var b=0;b<this.itemCount_;b++){var c=this.getFieldValue("RGB0");a.setAttribute("RGB"+b,c)}return a},domToMutation:function(a){for(var b=0;b<this.itemCount_;b++)this.removeInput("COLOR"+
 b);this.itemCount_=window.parseInt(a.getAttribute("items"),10);for(b=0;b<this.itemCount_;b++){var c=window.parseInt(a.getAttribute("RGB"+b),"#00ff00");this.appendDummyInput("COLOR"+b).setAlign(Blockly.ALIGN_RIGHT).appendField("Color "+(b+1)).appendField(new Blockly.FieldColour(c),"RGB"+b)}0==this.itemCount_&&this.appendDummyInput("COLOR0").setAlign(Blockly.ALIGN_RIGHT).appendField("Color 1").appendField(new Blockly.FieldColour("#00ff00"),"RGB0")},decompose:function(a){var b=Blockly.Block.obtain(a,
 "grove_rgb_led_container");b.initSvg();for(var c=b.getInput("STACK").connection,d=0;d<this.itemCount_;d++){var e=Blockly.Block.obtain(a,"grove_rgb_led_item");e.initSvg();c.connect(e.previousConnection);c=e.nextConnection}return b},compose:function(a){if(0==this.itemCount_)this.removeInput("COLOR0");else for(var b=this.itemCount_-1;0<=b;b--)this.removeInput("COLOR"+b);this.itemCount_=0;for(a=a.getInputTargetBlock("STACK");a;){b=this.getFieldValue("RGB"+this.itemCount_);null==b&&(b="00ff00");var c=
@@ -226,3 +228,102 @@ Blockly.Blocks['inout_tone'] = {
  this.setHelpUrl("");
   }
 };
+
+Blockly.Blocks['saveButton'] = {
+  init: function() {
+    this.onclick = save();
+  }
+};
+
+Blockly.Blocks['loadButton'] = {
+  init: function() {
+    this.onclick = loadFile();
+  }
+};
+
+/**
+* Save Arduino generated code to local file.
+*/
+function saveCode() {
+  var fileName = window.prompt('What would you like to name your file?', 'BlocklyDuino')
+  //doesn't save if the user quits the save prompt
+  if(fileName){
+    var blob = new Blob([Blockly.Arduino.workspaceToCode()], {type: 'text/plain;charset=utf-8'});
+    saveAs(blob, fileName + '.ino');
+  }
+}
+
+/**
+ * Save blocks to local file.
+ * better include Blob and FileSaver for browser compatibility
+ */
+function save() {
+  var xml = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+  var data = Blockly.Xml.domToText(xml);
+  var fileName = window.prompt('What would you like to name your file?', 'BlocklyDuino');
+  // Store data in blob.
+  // var builder = new BlobBuilder();
+  // builder.append(data);
+  // saveAs(builder.getBlob('text/plain;charset=utf-8'), 'blockduino.xml');
+  if(fileName){
+    var blob = new Blob([data], {type: 'text/xml'});
+    saveAs(blob, fileName + ".xml");
+  } 
+}
+
+/**
+ * Load blocks from local file.
+ */
+
+function loadFile() {
+ function load(event) {
+  var files = event.target.files;
+  // Only allow uploading one file.
+  if (files.length != 1) {
+    return;
+  }
+
+  // FileReader
+  var reader = new FileReader();
+  reader.onloadend = function(event) {
+    var target = event.target;
+    // 2 == FileReader.DONE
+    if (target.readyState == 2) {
+      try {
+        var xml = Blockly.Xml.textToDom(target.result);
+      } catch (e) {
+        alert('Error parsing XML:\n' + e);
+        return;
+      }
+      var count = Blockly.mainWorkspace.getAllBlocks().length;
+      if (count && confirm('Replace existing blocks?\n"Cancel" will merge.')) {
+        Blockly.mainWorkspace.clear();
+      }
+      Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xml);
+    }
+    // Reset value of input after loading because Chrome will not fire
+    // a 'change' event if the same file is loaded again.
+    document.getElementById('load').value = '';
+  };
+  reader.readAsText(files[0]);
+}
+
+/*
+ * auto save and restore blocks
+ */
+function auto_save_and_restore_blocks() {
+  // Restore saved blocks in a separate thread so that subsequent
+  // initialization is not affected from a failed load.
+  window.setTimeout(restore_blocks, 0);
+  // Hook a save function onto unload.
+  bindEvent(window, 'unload', backup_blocks);
+  tabClick(selected);
+
+  // Init load event.
+  var loadInput = document.getElementById('load');
+  loadInput.addEventListener('change', load, false);
+  document.getElementById('fakeload').onclick = function() {
+    loadInput.click();
+  };
+}
+}
