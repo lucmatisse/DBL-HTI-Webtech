962dbdda641ea721d5237edd4d544ad4dd2c87cd
isavgt <isavogten@gmail.com>
2022-06-06 16:39:10 +0200

    Added tone without time function

--- blockly/blocks_compressed.js
+++ blockly/blocks_compressed.js
@@ -208,8 +208,8 @@ this.setFieldValue(b,"VAR")},contextMenuType_:"variables_set",customContextMenu:
 Blockly.Blocks.variables_set={init:function(){this.jsonInit({message0:Blockly.Msg.VARIABLES_SET,args0:[{type:"field_variable",name:"VAR",variable:Blockly.Msg.VARIABLES_DEFAULT_NAME},{type:"input_value",name:"VALUE"}],previousStatement:null,nextStatement:null,colour:Blockly.Blocks.variables.HUE,tooltip:Blockly.Msg.VARIABLES_SET_TOOLTIP,helpUrl:Blockly.Msg.VARIABLES_SET_HELPURL});this.contextMenuMsg_=Blockly.Msg.VARIABLES_SET_CREATE_GET},getVars:function(){return[this.getFieldValue("VAR")]},renameVar:function(a,
 b){Blockly.Names.equals(a,this.getFieldValue("VAR"))&&this.setFieldValue(b,"VAR")},contextMenuType_:"variables_get",customContextMenu:Blockly.Blocks.variables_get.customContextMenu};
 
-// Block with tone as input instead of frequency
-Blockly.Blocks['inout_tone'] = {
+// Block with tone & time as input instead of frequency
+Blockly.Blocks['inout_tone_time'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("Set PIN#")
@@ -219,6 +219,21 @@ Blockly.Blocks['inout_tone'] = {
         .appendField("for")
         .appendField(new Blockly.FieldTextInput(""), "duration")
         .appendField("miliseconds");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Set the tone you want a speaker or buzzer to play (for example: C4)");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['inout_tone'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Set PIN#")
+        .appendField(new Blockly.FieldDropdown([["1","1"], ["2","2"], ["3","3"]]), "PIN")
+        .appendField("to tone")
+        .appendField(new Blockly.FieldTextInput(""), "tone_choice")
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(230);
