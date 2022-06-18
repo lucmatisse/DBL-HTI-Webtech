bb7fa5ef0ca2ef0244eb8767ecbb805accf3503a
Ilja <ilja.szegedi@gmail.com>
2022-06-18 15:53:14 +0200

    Added download cards

--- blockly/apps/blocklyduino/landingScript.js
+++ blockly/apps/blocklyduino/landingScript.js
@@ -2,3 +2,10 @@
  * This file implements the script for the landing page
  */
 
+/**
+ * Handles clicks on download cards
+ */
+function downloadClick(id) {
+  alert("Clicked card with ID: " + `"${id}"`);
+
+}
